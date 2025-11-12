b74abe17ab8bcf77ca75a34f638780e26f4c55eb
Alberto Araujo <alberto.z.araujo@aluno.senai.br>
2025-11-12 14:54:10 -0300

    feat: Add url_for in profile page

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('home.html')
+def homepage():
+    return render_template('homepage.html')
 
 @app.route('/profile/<username>')
-def perfil(username):
+def profile(username):
     return render_template("profile.html", username=username)
 
 if __name__ == '__main__':
