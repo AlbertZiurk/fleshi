5badee412c3983cb37cdcd95af90cbb7d36f6c17
Alberto <alberto.z.araujo@aluno.senai.br>
2025-12-03 15:11:13 -0300

    feat: add the future to post a photo

---
+++ appfleshi/models.py
@@ -0,0 +1,25 @@
+from time import timezone
+from email.policy import default
+from zoneinfo import ZoneInfo
+
+from datetime import  datetime, timezone
+
+from appfleshi import database, login_manager
+from flask_login import UserMixin
+
+class User(database.Model, UserMixin):
+  id = database.Column(database.Integer, primary_key=True)
+  username = database.Column(database.String(20), unique=True, nullable=False)
+  email = database.Column(database.String(100), unique=True, nullable=False)
+  password = database.Column(database.String(60), nullable=False)
+  photos = database.relationship('Photo', backref='user', lazy=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Photo(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    file_name = database.Column(database.String(255), unique=True, nullable=False, default='default.png')
+    upload_date = database.Column(database.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
